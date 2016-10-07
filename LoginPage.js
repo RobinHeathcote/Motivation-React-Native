8f2595732005c8050bada6c7f7ed8d38b539e94c
Robin Heathcote <robin@createk.io>
2016-10-07 15:11:56 +0100

    Initial commit

---
+++ LoginPage.js
@@ -0,0 +1,8 @@
+'use strict'
+
+import React, { Component } from 'react';
+import { NavigatorIOS, AppRegistry, StyleSheet, Text, TextInput, View,
+  TouchableHighlight, ActivityIndicator, Image }
+  from 'react-native';
+
+  
---
+++ index.ios.js
@@ -0,0 +1,34 @@
+'use strict'
+
+import React, { Component } from 'react';
+import {
+  NavigatorIOS,
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class Motivation extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: LoginPage,
+      }}/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
+
+AppRegistry.registerComponent('Motivation', () => Motivation);
